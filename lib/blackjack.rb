477dc26ca7f291be84e676ab31e99f261d01d82f
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-14 01:42:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,10 +30,9 @@ end
 
 def initial_round
   # code #initial_round here
-  deal_card + deal_card
-  display_card_total
+  sum = deal_card + deal_card
+  display_card_total(sum )
 end
-
 def hit?
   # code hit? here
 end
